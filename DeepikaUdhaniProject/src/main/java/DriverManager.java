2fc6e981188e5268874477f993807eefe4113def
DeepikaUdhani <deepika.udhani@gmail.com>
2021-09-05 22:01:27 +0100

    test

---
+++ DeepikaUdhaniProject/src/main/java/DriverManager.java
@@ -0,0 +1,31 @@
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.utils.ConfigProvider;
+
+public class DriverManager {
+
+	private WebDriver driver;
+
+	public WebDriver getDriver() {
+		if (driver == null) {
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "//src//test//resources//drivers//chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			return driver;
+		}
+		return driver;
+	}
+
+	public void launchApplication() {
+		getDriver().get(ConfigProvider.getProperty("url"));
+	}
+
+}
